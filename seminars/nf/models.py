58971fce5ef7d3ee735e9f4f6e2c3f71e8cd6d26
Artem Ryzhikov <artemryzhikoff@yandex.ru>
2019-11-27 12:23:29 +0000

    NF added

---
+++ seminars/nf/models.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+
+
+class NormalizingFlowModel(nn.Module):
+
+    def __init__(self, dim, prior, flows):
+        super().__init__()
+        self.dim = dim
+        self.prior = prior
+        self.flows = nn.ModuleList(flows)
+
+    def forward(self, x):
+        m, _ = x.shape
+        log_det = torch.zeros(m)
+        for flow in self.flows:
+            x, ld = flow.forward(x)
+            log_det += ld
+        z, prior_logprob = x, self.prior.log_prob(x)
+        return z, prior_logprob, log_det
+
+    def backward(self, z):
+        m, _ = z.shape
+        log_det = torch.zeros(m)
+        for flow in self.flows[::-1]:
+            z, ld = flow.backward(z)
+            log_det += ld
+        x = z
+        return x, log_det
+
+    def sample(self, n_samples):
+        z = self.prior.sample((n_samples,self.dim))
+        if z.dim() != 2: z = self.prior.sample((n_samples,))
+        x, _ = self.backward(z)
+        return x
+
